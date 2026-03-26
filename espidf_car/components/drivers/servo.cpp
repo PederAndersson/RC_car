6d663f0a549455db4982e0a66e1b8eeeb6497e62
Peder Andersson <peder.andersson@live.se>
2026-03-26 14:10:30 +0100

    servo working needs calibrating

---
+++ espidf_car/components/drivers/servo.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include "servo.h"
+#include "driver/ledc.h"
+#include "esp_check.h"
+#include "esp_err.h"
+
+static const char* TAG = "Servo";
+namespace Driver {
+
+    Sg90::Sg90(const ServoConfig& config) :
+        config_(config),
+        initialized_(false),
+        current_angle_(90)
+    
+    {}
+
+    esp_err_t Sg90::init(){
+        esp_err_t err = gpio_set_direction(config_.pwm_pin, GPIO_MODE_OUTPUT);
+        ESP_RETURN_ON_ERROR(err, TAG, "gpio_set_direction failed");
+        ledc_timer_config_t timer_cfg {
+            .speed_mode = config_.mode,
+            .duty_resolution = config_.duty_res,
+            .timer_num = config_.timer,
+            .freq_hz = config_.pwm_freq_hz,
+            .clk_cfg = LEDC_AUTO_CLK,
+            .deconfigure = false
+        };
+        err = ledc_timer_config(&timer_cfg);
+        ESP_RETURN_ON_ERROR(err, TAG, "ledc_timer_config() failed");
+        ledc_channel_config_t channel_cfg {
+            .gpio_num = config_.pwm_pin,
+            .speed_mode = config_.mode,
+            .channel = config_.channel,
+            .intr_type = LEDC_INTR_DISABLE,
+            .timer_sel = config_.timer,
+            .duty = 0,
+            .hpoint = 0,
+            .sleep_mode = LEDC_SLEEP_MODE_NO_ALIVE_NO_PD,
+            .flags = {0} 
+        };
+        err = ledc_channel_config(&channel_cfg);
+        ESP_RETURN_ON_ERROR(err, TAG, "ledc_channel_config failed");
+        initialized_ = true;
+        err = set_angle(90);
+        ESP_RETURN_ON_ERROR(err,TAG,"set_angle failed");
+        return ESP_OK;
+    }
+
+    esp_err_t Sg90::set_angle(uint8_t angle){
+        if (!initialized_) {return ESP_ERR_INVALID_STATE;}
+        if (config_.pwm_freq_hz == 0 || config_.max_angle <= config_.min_angle) {return ESP_ERR_INVALID_ARG;}
+        if (config_.max_pulse_us <= config_.min_pulse_us) {return ESP_ERR_INVALID_ARG;}
+        const uint32_t period_us = 1000000u / config_.pwm_freq_hz;
+        const uint32_t max_duty = (1u << static_cast<uint32_t>(config_.duty_res)) -1u;
+        const uint32_t angle_clamp = std::min<uint32_t>(config_.max_angle,
+             std::max<uint32_t>(config_.min_angle, angle));
+        const uint32_t pulse_us = config_.min_pulse_us + 
+                    ((angle_clamp - config_.min_angle)*(config_.max_pulse_us - config_.min_pulse_us)) /
+                    (config_.max_angle - config_.min_angle);
+        const uint32_t duty = ((pulse_us * max_duty + (period_us / 2u)) / period_us);
+        esp_err_t err = ledc_set_duty(config_.mode, config_.channel, duty);
+        ESP_RETURN_ON_ERROR(err,TAG, "ledc_set_duty failed");
+        err = ledc_update_duty(config_.mode, config_.channel);
+        ESP_RETURN_ON_ERROR(err,TAG,"ledc_update_duty failed");
+        current_angle_ = angle_clamp;
+        return ESP_OK;
+    }
+}
--- espidf_car/main/app_main.cpp
+++ espidf_car/main/app_main.cpp
@@ -2,6 +2,9 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "dc_motor.h"
+#include "hal/ledc_types.h"
+#include "servo.h"
+#include "esp_check.h"
 
 const Driver::MotorConfig motor_config{
     .motor = {
@@ -16,45 +19,57 @@ const Driver::MotorConfig motor_config{
     .pwm_freq_hz = 1000
 };
 
-Driver::L298N motor(motor_config);
+const Driver::ServoConfig servo_cfg{
+    .pwm_pin = GPIO_NUM_5,
+    .timer = LEDC_TIMER_1,
+    .channel = LEDC_CHANNEL_1,
+    .mode = LEDC_LOW_SPEED_MODE,
+    .duty_res = LEDC_TIMER_14_BIT,
+    .pwm_freq_hz = 50,
+    .min_pulse_us = 1000, //500
+    .max_pulse_us = 2000, //2400
+    .min_angle = 0,
+    .max_angle = 180
+};
 
+Driver::L298N motor(motor_config);
+Driver::Sg90 servo(servo_cfg);
 
 static const char* TAG = "RC_CAR";
 
-extern "C" void app_main(void) {
+extern "C" esp_err_t app_main(void) {
     ESP_LOGI(TAG, "RC car boot ok");
     esp_err_t err = motor.init();
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "motor.init() failed : %s", esp_err_to_name(err));
-        return;
-    }
+    ESP_RETURN_ON_ERROR(err, TAG, "motor.init failed");
+    err = servo.init();
+    ESP_RETURN_ON_ERROR(err,TAG,"servo.init failed");
     while (true) {
         ESP_LOGI(TAG, "Forward 75%%");
         err = motor.set_throttle_percent(75);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "set_throttle_percent failed: %s", esp_err_to_name(err));
-        }
+        ESP_RETURN_ON_ERROR(err,TAG,"motor.set_trottle failed");
+        err = servo.set_angle(45);
+        ESP_RETURN_ON_ERROR(err,TAG,"servo.set_angle failed");
         vTaskDelay(pdMS_TO_TICKS(2000));
 
         ESP_LOGI(TAG, "Stop");
         err = motor.set_throttle_percent(0);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "set_throttle_percent failed: %s", esp_err_to_name(err));
-        }
+        ESP_RETURN_ON_ERROR(err,TAG,"motor.set_throttle failed");
+        err = servo.set_angle(90);
+        ESP_RETURN_ON_ERROR(err,TAG,"servo.set_angle failed");
         vTaskDelay(pdMS_TO_TICKS(1000));
 
         ESP_LOGI(TAG, "Reverse 75%%");
         err = motor.set_throttle_percent(-75);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "set_throttle_percent failed: %s", esp_err_to_name(err));
-        }
+        ESP_RETURN_ON_ERROR(err,TAG,"motor.set_throttle failed");
+        err = servo.set_angle(135);
+        ESP_RETURN_ON_ERROR(err,TAG,"servo.set_angle failed");
         vTaskDelay(pdMS_TO_TICKS(2000));
 
         ESP_LOGI(TAG, "Stop");
         err = motor.set_throttle_percent(0);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "set_throttle_percent failed: %s", esp_err_to_name(err));
-        }
+        ESP_RETURN_ON_ERROR(err,TAG,"motor.set_throttle failed");
+        err = servo.set_angle(90);
+        ESP_RETURN_ON_ERROR(err,TAG,"servo.set_angle failed");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 
