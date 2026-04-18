1c36027f1288fc3ac0f529c874c8dc16d6b5c383
Peder Andersson <peder.andersson@live.se>
2026-04-18 22:18:50 +0200

    esp_now is working and transmitting, fixed git ignore so it covers entire project, stopped tracking missed files from previous commit

--- espidf_controller/main/app_main.cpp
+++ espidf_controller/main/app_main.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "freertos/projdefs.h"
+#include "send.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_check.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-extern "C" void app_main(void)
-{
+static const char* TAG = "controller";
+
+extern "C" void app_main(void) {
+    ESP_LOGI(TAG, "Controller boot ok");
+    ESP_RETURN_VOID_ON_ERROR(controller_init(), TAG, "controller init failed");
+    Car_input input{};
+    input.steering = 90;
+    input.throttle = 50;
+
+    while(true){
+        send_input(&input);
+        vTaskDelay(pdMS_TO_TICKS(20));
+    }
 
 }
